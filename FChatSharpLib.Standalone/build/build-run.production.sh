fb3e431d4cd564dff1d45a3b73b0c24b24f671bc
Aelith Blanchette <tonyteur@yahoo.fr>
2021-09-25 02:56:17 -0400

    Added build scripts

---
+++ FChatSharpLib.Standalone/build/build-run.production.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh ./build-run.sh Production
---
+++ FChatSharpLib.Standalone/build/build-run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+environment=$1
+lower_environment=$(echo "$environment" | awk '{print tolower($0)}')
+cwd="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+#Updating
+git pull
+
+#Compiling .NET Host
+cd $cwd/..
+dotnet publish -c Release -r linux-x64
+
+# Creating required directories
+mkdir -p /var/www/vc/$lower_environment/fchatbot/
+
+# Stop running service
+service fchatbot.$lower_environment stop
+
+# Removing files from destination
+rm -rf /var/www/vc/$lower_environment/fchatbot/*
+
+# Copying files to destination
+cp -rf $cwd/../bin/Release/net5.0/linux-x64/publish/* /var/www/vc/$lower_environment/fchatbot/
+
+# ONCE - Copy service files
+#cp -rf $cwd/../config/systemd/*$lower_environment*.service /etc/systemd/system/
+
+#Return to initial directory
+cd $cwd
+
+# ONCE - Enable new Service files and Apache files
+#systemctl daemon-reload
+
+# Start service
+service fchatbot.$lower_environment start
