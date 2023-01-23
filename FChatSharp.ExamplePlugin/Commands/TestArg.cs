8f420923a6d0fb2662e6448e048e8b1312e632db
Elise Pariat <tonyteur@yahoo.fr>
2023-01-23 21:56:13 +0100

    beta12 with args fixed

---
+++ FChatSharp.ExamplePlugin/Commands/TestArg.cs
@@ -0,0 +1,20 @@
+﻿using FChatSharpLib.Entities.Plugin;
+using FChatSharpLib.Entities.Plugin.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FChatSharp.ExamplePlugin.Commands
+{
+    public class TestArg : BaseCommand<ExamplePlugin> 
+    {
+        public override string Description => "Replies with the passed argument.";
+
+        public override async Task ExecuteCommand(string character, IEnumerable<string> args, string channel)
+        {
+            Plugin.FChatClient.SendMessageInChannel(args.JoinAsString("---"), channel);
+        }
+    }
+}
